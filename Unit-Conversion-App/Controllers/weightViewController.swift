d93a8ccea499842654be7f058a3ac86e70f338d4
SE <se@imac-09.local>
2018-08-16 21:38:06 +0530

    Added controllers and models and complete UI

---
+++ Unit-Conversion-App/Controllers/weightViewController.swift
@@ -0,0 +1,27 @@
+//
+//  weightViewController.swift
+//  Unit-Conversion-App
+//
+//  Created by SE on 8/16/18.
+//  Copyright © 2018 SE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class weightViewController : UIViewController {
+    
+    @IBOutlet weak var gram_text_field: UITextField!
+    @IBOutlet weak var kilogram_text_field: UITextField!
+    @IBOutlet weak var pound_text_field: UITextField!
+    @IBOutlet weak var ounce_text_field: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+}
